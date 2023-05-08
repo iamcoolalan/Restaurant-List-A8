73a8a0c934df080844e4fd5503df797b551337c5
iamcoolalan <iamalan1996@gmail.com>
2023-05-08 15:27:40 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,32 +1,15 @@
 //require packages used in the project
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 const routes = require('./routes')
+require('./config/mongoose')
 
 //start express and set the port
 const app = express()
 const port = 3000
-
-//setting mongoDB connection
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-//use env to protect your personal info
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 //setting template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,34 +1,11 @@
-const mongoose = require('mongoose')
 const Restaurants = require('../restaurants')
 const restaurantsList = require('../../restaurant.json')
+const db = require('../../config/mongoose')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 
 db.once('open', () => {
-  console.log('mongodb connected!')
-
   restaurantsList.results.forEach(restaurant => {
-    Restaurants.create({
-      name: restaurant.name,
-      name_en: restaurant.name_en,
-      category: restaurant.category,
-      image: restaurant.image,
-      location: restaurant.location,
-      phone: restaurant.phone,
-      google_map: restaurant.google_map,
-      rating: restaurant.rating,
-      description: restaurant.description
-    })
+    Restaurants.create(restaurant)
   })
 
   console.log('done')
